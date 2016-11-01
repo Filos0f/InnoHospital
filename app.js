8ba54979e6c4b4b3abb12bda7a0b04c72f67cef8
KorsarU <KorsarUTrue@gmail.com>
2016-11-01 23:49:47 +0300

    Fedor version

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ app.get('/patient', function(req, res){
 	res.render('patient');
 });
 
+app.get('/signinStaff', function(req, res){
+	res.render('StaffMain');
+});
+
+app.get('/staff', function(req, res){
+						   res.render('staff');
+						   });
+
 app.post('/add',function(req,res){
 	pg.connect(connect,function(err,client,done){
 		if(err){
@@ -37,6 +45,76 @@ app.post('/add',function(req,res){
 done();
 res.redirect('/');
 	});
+});
+
+app.post('/log', function(req, res){
+	console.log("-------------LOG -----------");
+	
+	const pg = require('pg');
+	const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5433/InnoHospital';
+	const client = new pg.Client(connectionString);
+	client.connect();
+
+	var sqlQuery = 'SELECT * from person where email = \'' + req.body.email + 
+	'\' and hashpassword = \'' + req.body.hashpassword + "\'";
+
+	console.log(sqlQuery);
+
+	function Autorization() { this.value = 0; }
+	var isAutorize = new Autorization();
+	const query = client.query(sqlQuery, function(err, result) {
+		console.log(result.rows.length);
+		if(result.rows.length != 0)
+		{
+			res.render('patient_cabinet');
+		}
+		else
+		{
+			res.redirect('/patient');
+		}
+	});
+});
+
+app.get('/registration', function(req, res){
+	console.log("------------- REGISTRATION -----------");
+	res.render('registration');
+});
+
+app.post('/addPatient',function(req,res){
+
+	console.log("-------------ADD -----------");
+	console.log(req.body.gender);
+
+	const pg = require('pg');
+	const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5433/InnoHospital';
+	const client = new pg.Client(connectionString);
+	client.connect();
+
+	var hashSolt = 2132343;
+	var hashpassword = 2132343 * 2132343;
+
+	var sqlQuery = 'INSERT INTO person VALUES(' + req.body.passport + ', ' + 
+	req.body.fname + ', ' + req.body.sname + ', ' +
+	req.body.city + ' ' + req.body.street + ' ' + req.body.appartment +
+	+ ', ' + eq.body.email + ', ' + eq.body.phone + ', ' + eq.body.birthdate +
+	', ' + eq.body.gender + ', ' + hashpassword + ', ' + hashSolt;
+
+	console.log(sqlQuery);
+
+	client.query(sqlQuery);
+
+	/*
+	
+	pg.connect(connect,function(err,client,done){
+		if(err){
+			return console.error('Error fetching from pool',err);
+	}
+	client.query("INSERT INTO recipes(name,ingredients,directions) VALUES($1,$2,$3)",
+		[req.body.name,req.body.ingredients,req.body.directions]);
+	done();
+	res.redirect('/log');
+	});
+	*/
 });
 
 
