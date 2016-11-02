742715521f94afb2ef4060d5257d08828f92cbf2
Kryukova Lida <Lidakrukova1994@gmail.com>
2016-11-02 14:37:08 +0700

    security

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ bodyParser=require('body-parser'),
 cons=require('consolidate'),
 dust=require('dustjs-helpers'),
 pg=require('pg'),
+md5=require('js-md5'),
 app=express();
-var connect="postgres://postgres:1@localhost:5433/InnoHospital";
+var connect="postgres://lida:is241s05@localhost:5432/InnoHospital";
 //Assign DUST engine to .dust files
 app.engine('dust',cons.dust);
 //Set deafalt ext
@@ -34,12 +35,11 @@ app.post('/log', function(req, res){
 	console.log("-------------LOG -----------");
 	
 	const pg = require('pg');
-	const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5433/InnoHospital';
+	const connectionString = process.env.DATABASE_URL || 'postgres://lida:is241s05@localhost:5432/LidaDataBase';
 	const client = new pg.Client(connectionString);
 	client.connect();
 
-	var sqlQuery = 'SELECT * from person where email = \'' + req.body.email + 
-	'\' and hashpassword = \'' + req.body.hashpassword + "\'";
+	var sqlQuery = 'SELECT * from person where email = \'' + req.body.email + '\'';
 
 	console.log(sqlQuery);
 
@@ -49,6 +49,10 @@ app.post('/log', function(req, res){
 		console.log(result.rows.length);
 		if(result.rows.length != 0)
 		{
+			//var obj = JSON.parse(result.rows);
+			console.log(JSON.stringify(result.rows[0]).substring(JSON.stringify(result.rows[0]).indexOf('hashsalt:')+11, JSON.stringify(result.rows[0]).indexOf('hashsalt')));
+		
+			console.log(obj);
 			res.render('patient_cabinet');
 		}
 		else
@@ -69,23 +73,37 @@ app.post('/addPatient',function(req,res){
 	console.log(req.body.gender);
 
 	const pg = require('pg');
-	const connectionString = process.env.DATABASE_URL || 'postgres://postgres:1@localhost:5433/InnoHospital';
+	const connectionString = process.env.DATABASE_URL || 'postgres://lida:is241s05@localhost:5432/LidaDataBase';
 	const client = new pg.Client(connectionString);
 	client.connect();
 
-	var hashSolt = 2132343;
-	var hashpassword = 2132343 * 2132343;
+//	var hashSolt = 2132343;
 
-	var sqlQuery = 'INSERT INTO person VALUES(' + req.body.passport + ', ' + 
-	req.body.fname + ', ' + req.body.sname + ', ' +
+	function hash(key)
+	{
+	    var h = 0;
+	    
+	    for (p = 0; p != key.length; p++) {
+    		h = h * 31 + key.charAt(p);
+	    }
+	    return h;
+	}
+
+	var hashSolt = hash(req.body.passport);
+	console.log(req.body.appartment);
+	console.log(req.body.email);
+
+	var sqlQuery = 'INSERT INTO person VALUES (\'' + req.body.passport + '\', \'' + 
+	req.body.fname + '\', \'' + req.body.sname + '\', \'' +
 	req.body.city + ' ' + req.body.street + ' ' + req.body.appartment +
-	+ ', ' + eq.body.email + ', ' + eq.body.phone + ', ' + eq.body.birthdate +
-	', ' + eq.body.gender + ', ' + hashpassword + ', ' + hashSolt;
+	'\', \'' + req.body.email + '\', \'' + req.body.phone + '\', \'' + req.body.birthdate +
+	'\', ' + false + ',\'' + md5(req.body.hashpassword + hashSolt) + '\',\'' + hashSolt + '\')';
+	
 
 	console.log(sqlQuery);
-
 	client.query(sqlQuery);
-
+	
+	res.render('patient');
 	/*
 	
 	pg.connect(connect,function(err,client,done){
@@ -94,12 +112,17 @@ app.post('/addPatient',function(req,res){
 	}
 	client.query("INSERT INTO recipes(name,ingredients,directions) VALUES($1,$2,$3)",
 		[req.body.name,req.body.ingredients,req.body.directions]);
-	done();
+	done(
 	res.redirect('/log');
 	});
 	*/
+	
 });
 
+app.get('/room', function(reg, res) {
+	console.log('----------------to Room --------------------');
+	res.render('/pathient_cabinet');
+});
 
 app.listen(3000,function(){
 	console.log('Server started on 3000 port');
