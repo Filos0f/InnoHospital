0d026ef6a36bf6aa8e8cefb16bf55c96b9f537f6
Kryukova Lida <Lidakrukova1994@gmail.com>
2016-11-13 03:31:19 +0700

    sql replace

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,64 +1,63 @@
 ﻿create table Person (
-idPassport varchar(256),
-firstName varchar(256) NOT NULL,
-secondName varchar(256) NOT NULL,
-address varchar(256) NOT NULL,
-email varchar(256) NOT NULL,
-telN varchar(256) NOT NULL,
-birthDay Date NOT NULL, 
-gender varchar(1) NOT NULL,
-hashPassword varchar(256) NOT NULL,
-hashSalt varchar(256) NOT NULL,
-Primary key(idPassport)  
+	idPassport varchar(256),
+	firstName varchar(256) NOT NULL,
+	secondName varchar(256) NOT NULL,
+	address varchar(256) NOT NULL,
+	email varchar(256) NOT NULL,
+	telN varchar(256) NOT NULL,
+	birthDay Date NOT NULL, 
+	gender varchar(20) NOT NULL,
+	hashPassword varchar(256) NOT NULL,
+	hashSalt varchar(256) NOT NULL,
+	Primary key(idPassport)  
 );
 
 create table Positions (
-idPos int NOT NULL,
-title varchar(256) NOT NULL,
-idEmp varchar(256),
-Primary key (idPos)
+	idPos int NOT NULL,
+	title varchar(256) NOT NULL,
+	idEmp varchar(256),
+	Primary key (idPos)
 );
 
 create table Employee (
-rating int NOT NULL,
-idPos int NOT NULL,
-idEmp varchar(256) NOT NULL,
-roomN int NOT NULL,
-idPassport varchar(256),
-Primary key (idPassport),
-Foreign key (idPassport) references Person,
-Foreign key (idPos) references Positions
+	rating int NOT NULL default(0),
+	idPos int NOT NULL,
+	idEmp varchar(256) NOT NULL,
+	roomN int NOT NULL,
+	idPassport varchar(256),
+	Primary key (idPassport),
+	Foreign key (idPassport) references Person,
+	Foreign key (idPos) references Positions
 );
 
 
 create table Patient (
-idIP varchar(256) NOT NULL,
-telFamily varchar(256) NOT NULL,
-idPassport varchar(256),
-Primary key (idPassport),
-Foreign key (idPassport) references Person
+	idIP varchar(256) NOT NULL,
+	telFamily varchar(256) NOT NULL,
+	idPassport varchar(256),
+	Primary key (idPassport),
+	Foreign key (idPassport) references Person
 );
 
-ALTER TABLE Positions
-ADD CONSTRAINT PosForeignKey Foreign key (idEmp) references Employee;
+ALTER TABLE Positions ADD CONSTRAINT PosForeignKey Foreign key (idEmp) references Employee;
 
 create table WorkingSchedule (
-roomN int NOT NULL,
-startTime time,
-finishTime time,
-day date,
-Primary key(startTime, finishTime, day)
+	roomN int NOT NULL,
+	startTime time,
+	finishTime time,
+	day date,
+	Primary key(startTime, finishTime, day)
 );
 
 
 create table isOpen (
-idEmp varchar(256),
-startTime time,
-finishTime time,
-day date,
-Primary key (idEmp, startTime, finishTime, day),
-Foreign key (idEmp) references Employee,
-Foreign key (startTime, finishTime, day) references WorkingSchedule
+	idEmp varchar(256),
+	startTime time,
+	finishTime time,
+	day date,
+	Primary key (idEmp, startTime, finishTime, day),
+	Foreign key (idEmp) references Employee,
+	Foreign key (startTime, finishTime, day) references WorkingSchedule
 );
 
 create table VisitSchedule (
@@ -67,16 +66,23 @@ create table VisitSchedule (
 	offsetTime time,
 	idIP varchar(256),
 	idEmp varchar(256),
-	evoluation int,
+	evoluation boolean default(false),
 	primary key (idIP, idEmp),
 	foreign key (idIP) references Patient,
 	foreign key (idEmp) references Employee
 );
 
+create table ConclusionTypes (
+	idType varchar(256) NOT NULL,
+	title varchar(256) NOT NULL,
+	primary key(idType)
+);
+
 create table Conclusion (
-	type varchar(256) NOT NULL,
+	idType varchar(256) NOT NULL,
 	id int,
-	primary key(id)
+	primary key(id),
+	foreign key (idType) references ConclusionTypes 
 );
 
 create table Result (
@@ -90,41 +96,43 @@ create table Result (
 	foreign key (idEmp) references Employee
 );
 
-create table generalizedAnalysis (
-	boolResult boolean,
+create table XRay (
+	scan bit(2048),
+	description varchar(256),
 	id int,
 	primary key (id),
-	foreign key (id) references conclusion
+	foreign key (id) references Conclusion
+);
+
+create table generalizedAnalysisTitles (
+	idTitle varchar(256),
+	title varchar(256),
+	standard real,
+	primary key(idTitle)
+);
+
+create table generalizedAnalysis (
+	idTitle varchar(256),
+	id int,
+	result real,
+	primary key (id),
+	foreign key (id) references conclusion,
+	foreign key (idTitle) references generalizedAnalysisTitles
 );
 
 create table diagnosis (
 	idTitle int,
+	nationalCode varchar(20),
 	id int,
 	primary key (id),
-	foreign key (id) references conclusion
-);
-
-create table XRay (
-	scan bit(1024),
-	description varchar(256),
-	id int,
-	primary key (id),
-	foreign key (id) references conclusion
-);
-
-create table Blood (
-	testType varchar(256),
-	id int,
-	standard real,
-	result real,
-	primary key (id),
-	foreign key (id) references conclusion
+	foreign key (id) references Conclusion
 );
 
 create table DiagnosisInfo (
 	title varchar(256),
 	idTitle int,
-	rate real,
+	nationalCode varchar(20),
+	rate real default(0),
 	primary key (idTitle)
 );
 
